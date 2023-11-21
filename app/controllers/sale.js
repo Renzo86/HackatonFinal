e752b7402daa416787b5d82eab1f1bd961dd8cec
Luis Bolivar <lbolivar20@gmail.com>
2023-11-21 13:54:41 -0500

    API - Mis Compras

--- app/controllers/sale.js
+++ app/controllers/sale.js
@@ -1,24 +1,109 @@
 const db = require("../models");
 const Sale = db.sale;
+const Detail = db.saleDetail;
+const User = db.user;
+const Product = db.product;
 
-exports.findOne = async (req, res) => {
+const { Op } = require("sequelize");
+
+// Todas las compras del usuario
+exports.findAll = async (req, res) => {
   const { id } = req.params;
-    Sale.findOne({
-      where: {
-          id
-      },
-      attributes: ['id', 'idUser','mtoTotal','tokenCulqiUser','tokenCulqiOrder','createdAt'],
-      include: [
-        { model: sale_detail, as: 'detail',
-          attributes: ['id', 'nombre','idProduct','quantity','valTotal'] 
-        }
-    ]
-  }).then(data => {
-      res.send(data);
-    }).catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error ocurred while retrieving sale."
-      });
+  Sale.findAll({
+    where: {
+      idUser: id
+    }
+}
+  ).then(data => {
+    res.send(data);
+  }).catch(err => {
+    res.status(500).send({
+      message:
+        err.message || "Some error ocurred while retrieving sales."
     });
+  });
+};
+
+// consulta de la compra
+exports.saleCab = async (req, res) => {
+  const { id } = req.params;
+  Sale.findOne({
+    where: {
+        id,
+    },
+    attributes: ['id', 'idUser','mtoTotal','tokenCulqiUser','tokenCulqiOrder','createdAt']
+}
+  ).then(data => {
+    res.send(data);
+  }).catch(err => {
+    res.status(500).send({
+      message:
+        err.message || "Some error ocurred while retrieving sales."
+    });
+  });
+};
+
+// consulta compras pendientes
+exports.saleCabPendiente = async (req, res) => {
+  const { id } = req.params;
+  Sale.findAll({
+    where: {
+      idUser: id,
+      tokenCulqiUser: null
+    },
+    attributes: ['id', 'idUser','mtoTotal','tokenCulqiUser','tokenCulqiOrder','createdAt']
+}
+  ).then(data => {
+    res.send(data);
+  }).catch(err => {
+    res.status(500).send({
+      message:
+        err.message || "Some error ocurred while retrieving sales."
+    });
+  });
+};
+
+// consulta compras completadas
+exports.saleCabCompletado = async (req, res) => {
+  const { id } = req.params;
+  Sale.findAll({
+    where: {
+      idUser: id,
+        tokenCulqiUser: {
+          [Op.not]: null
+        }
+    },
+    attributes: ['id', 'idUser','mtoTotal','tokenCulqiUser','tokenCulqiOrder','createdAt']
+}
+  ).then(data => {
+    res.send(data);
+  }).catch(err => {
+    res.status(500).send({
+      message:
+        err.message || "Some error ocurred while retrieving sales."
+    });
+  });
+};
+
+// consulta detalle compras
+exports.saleDet = async (req, res) => {
+  const { id } = req.params;
+  Detail.findAll({
+    include: [
+      { model: Product,
+        attributes: ['id','idCategory','description'] 
+      }
+  ],
+    where: {
+      idSale: id
+    }
+}
+  ).then(data => {
+    res.send(data);
+  }).catch(err => {
+    res.status(500).send({
+      message:
+        err.message || "Some error ocurred while retrieving sales det."
+    });
+  });
 };
--- app/routes/sale.js
+++ app/routes/sale.js
@@ -1,4 +1,8 @@
 const controller = require("../controllers/sale");
 module.exports = function(app) {
-  app.get("/api/sale/:id", controller.findOne);
+  app.get("/api/sale/:id", controller.findAll);
+  app.get("/api/sale/:id/complete", controller.saleCabCompletado);
+  app.get("/api/sale/:id/incomplete", controller.saleCabPendiente);
+  app.get("/api/sale/head/:id", controller.saleCab);
+  app.get("/api/sale/head/:id/detail", controller.saleDet);
 };
